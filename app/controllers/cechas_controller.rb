280109348acb794e0ce1be59e3fb13551915cb9a
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2011-05-06 21:27:02 +0200

    Kontrolery

---
+++ app/controllers/cechas_controller.rb
@@ -0,0 +1,7 @@
+class CechasController < ApplicationController
+
+  hobo_model_controller
+
+  auto_actions :all
+
+end
---
+++ app/controllers/dopasowanies_controller.rb
@@ -0,0 +1,7 @@
+class DopasowaniesController < ApplicationController
+
+  hobo_model_controller
+
+  auto_actions :all
+
+end
