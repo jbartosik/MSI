d20aa310dcd170c18dabe7d3b4a2b080bce6c8f1
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2011-05-06 20:03:12 +0200

    Model: swiat

---
+++ app/models/swiat.rb
@@ -0,0 +1,29 @@
+class Swiat < ActiveRecord::Base
+
+  hobo_model # Don't put anything above this
+
+  fields do
+    nazwa :string
+    opis  :string
+    timestamps
+  end
+
+  # --- Permissions --- #
+
+  def create_permitted?
+    acting_user.administrator?
+  end
+
+  def update_permitted?
+    acting_user.administrator?
+  end
+
+  def destroy_permitted?
+    acting_user.administrator?
+  end
+
+  def view_permitted?(field)
+    true
+  end
+
+end
